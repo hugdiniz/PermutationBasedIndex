8ecaf3b1232ab1fd3f76281140ab02f1eb314d5c
Hugo Rebelo <hugdiniz@gmail.com>
2018-01-19 16:58:51 -0200

    altering for execute 1º experiments

--- experiments/heuristic_retrieval.py
+++ experiments/heuristic_retrieval.py
@@ -27,11 +27,11 @@ if __name__ == '__main__':
     '''
 #    dataset_name = "psa"
 #     dataset_name = "pan10"
-#    dataset_name = "pan11"
+    dataset_name = "pan11"
 
-    dataset_name,sample_size = "pan10-%d-samples",10 
+#    dataset_name,sample_size = "pan10-%d-samples",10 
     dataset_name = dataset_name%(sample_size)
-    queries_percentage = 90
+    queries_percentage = 25
      
     cv_parameters = {
         "cv__analyzer" : ('word',),
@@ -196,12 +196,12 @@ if __name__ == '__main__':
     '''
         nearest neighbor search (ranking)
     '''
-
-    for i,linei in lshnns_df_paramaters.iterrows():
-        print("#"*10+" LSH N.N.S. "+"#"*10)
-        print(linei)
-        lsh_nearest_neighbors_search(dataset_name,linei,i,dataset_encoding)
-        print("-"*20)
+# 
+#     for i,linei in lshnns_df_paramaters.iterrows():
+#         print("#"*10+" LSH N.N.S. "+"#"*10)
+#         print(linei)
+#         lsh_nearest_neighbors_search(dataset_name,linei,i,dataset_encoding)
+#         print("-"*20)
 
 #     for i,linei in nns_df_paramaters.iterrows():
 #         print("#"*10+" N.N.S. "+"#"*10)
@@ -236,91 +236,91 @@ if __name__ == '__main__':
     today = today.strftime('%Y-%m-%d_%H-%M-%S_')
     
     
-    '''
-        logging LSH nearest neighbors results on csv
-    '''
-    a = pd.merge(cv_df_paramaters, lsht_df_paramaters, how='inner', left_index=True, right_on=['input__filename_index',],)
-    b = pd.merge(a, lshnns_df_paramaters, how='inner', left_index=True, right_on=['input__filename_index',],suffixes=('_lsht','_lshtnns'))    
-    del a
-
-    for rowi in b.iterrows():
-        cv_index = rowi[1]['input__filename_index_lsht']
-        lsht_index = rowi[1]['input__filename_index_lshtnns']
-        nns_index = rowi[0]
-        
-#         print(cv_index,'-',lsht_index,'-',nns_index)
-        cv_file_path = h5_results_filename(dataset_name, 'cv', cv_index).replace('results','time')
-        lsht_file_path = h5_results_filename(dataset_name, 'lsht', lsht_index).replace('results','time')
-        lshnns_file_path = h5_results_filename(dataset_name, 'lshnns', nns_index).replace('results','results_evaluation')
-        lshnns_time_file_path = h5_results_filename(dataset_name, 'lshnns', nns_index).replace('results','time')
-
-#         print('\t',cv_file_path)
-#         print('\t',lsht_file_path)
-#         print('\t',lshnns_file_path)
-#         print('=========')
-        approach_precisions = hdf_to_sparse_matrix('precisions', lshnns_file_path)
-        approach_recalls = hdf_to_sparse_matrix('recalls', lshnns_file_path)
-        average_precision = hdf_to_sparse_matrix('average_precisions', lshnns_file_path).todense()
-        
-        b.loc[nns_index,'MAP'] = average_precision.mean()
-        b.loc[nns_index,'MAP_std'] = average_precision.std()
-        b.loc[nns_index,'precision_recall_path'] = lshnns_file_path
-        b.loc[nns_index,'recall_mean'] = approach_recalls[:,-1].todense().mean()
-        b.loc[nns_index,'recall_std'] = approach_recalls[:,-1].todense().std()
-        b.loc[nns_index,'precision_mean'] = approach_precisions[:,-1].todense().mean()
-        b.loc[nns_index,'precision_std'] = approach_precisions[:,-1].todense().std()
-        
-        del approach_precisions, approach_recalls, average_precision
-
-        b.loc[nns_index,'documents_count'] = documents_count
-        b.loc[nns_index,'queries_count'] = queries_count
-        
-        with open(cv_file_path.replace('time.h5', 'vocabulary.pkl'),'rb') as f:
-            b.loc[nns_index,'vocabulary_size'] = len(pickle.load(f))
-
-        q = hdf_to_sparse_matrix('queries', lsht_file_path.replace('time','results'))
-        b.loc[nns_index,'lsht_features'] = q.shape[1]
-        del q
-        
-        b.loc[nns_index,'indexing_mean_time'] = 0
-        b.loc[nns_index,'querying_mean_time'] = 0
-        
-        cv_time_dataframe = pd.read_hdf(cv_file_path, 'time_dataframe')
-        b.loc[nns_index,'cv_documents_mean_time'] = cv_time_dataframe.loc[0,'documents_mean_time'] 
-        b.loc[nns_index,'cv_queries_mean_time'] = cv_time_dataframe.loc[0,'queries_mean_time']
-                
-        b.loc[nns_index,'indexing_mean_time'] += b.loc[nns_index,'cv_documents_mean_time']
-        b.loc[nns_index,'querying_mean_time'] += b.loc[nns_index,'cv_queries_mean_time']
-        del cv_time_dataframe 
-
-        d_time = hdf_to_sparse_matrix('documents_time',lsht_file_path)
-        q_time = hdf_to_sparse_matrix('queries_time',lsht_file_path)
-        
-        b.loc[nns_index,'lsht_documents_mean_time'] = d_time.sum(axis=1).mean() 
-        b.loc[nns_index,'lsht_queries_mean_time'] = q_time.sum(axis=1).mean()
-
-        b.loc[nns_index,'indexing_mean_time'] += b.loc[nns_index,'lsht_documents_mean_time']
-        b.loc[nns_index,'querying_mean_time'] += b.loc[nns_index,'lsht_queries_mean_time']
-        del d_time, q_time 
-
-        nns_time_dataframe = pd.read_hdf(lshnns_time_file_path, 'time_dataframe')
-        b.loc[nns_index,'nns_documents_mean_time'] = nns_time_dataframe.loc[0,'documents_mean_time'] 
-        b.loc[nns_index,'nns_queries_mean_time'] = nns_time_dataframe.loc[0,'queries_mean_time']
-
-        b.loc[nns_index,'indexing_mean_time'] += b.loc[nns_index,'nns_documents_mean_time']
-        b.loc[nns_index,'querying_mean_time'] += b.loc[nns_index,'nns_queries_mean_time']
-        del nns_time_dataframe
-
-        print(b.loc[nns_index,'lsht__selection_function'],' : ',int(b.loc[nns_index,'lsht_features']),' features x ',b.loc[nns_index,'lsht__n_permutations'],' permutation')
-        print("MAP = %4.2f[+-%4.2f]"%(b.loc[nns_index,'MAP'],b.loc[nns_index,'MAP_std']))
-        print("recall = %4.2f[+-%4.2f]"%(b.loc[nns_index,'recall_mean'],b.loc[nns_index,'recall_std']))
-        print("index time = %4.4f"%(b.loc[nns_index,'cv_documents_mean_time']+b.loc[nns_index,'lsht_documents_mean_time']+b.loc[nns_index,'nns_documents_mean_time']))
-        print("query time = %4.4f"%(b.loc[nns_index,'cv_queries_mean_time']+b.loc[nns_index,'lsht_queries_mean_time']+b.loc[nns_index,'nns_queries_mean_time']))
-         
-        print("---->",b.loc[nns_index,'indexing_mean_time'])
-    b.to_csv('%s%s_lsh_results.csv'%(today,dataset_name),sep='\t')
-    
-    del b
+#     '''
+#         logging LSH nearest neighbors results on csv
+#     '''
+#     a = pd.merge(cv_df_paramaters, lsht_df_paramaters, how='inner', left_index=True, right_on=['input__filename_index',],)
+#     b = pd.merge(a, lshnns_df_paramaters, how='inner', left_index=True, right_on=['input__filename_index',],suffixes=('_lsht','_lshtnns'))    
+#     del a
+# 
+#     for rowi in b.iterrows():
+#         cv_index = rowi[1]['input__filename_index_lsht']
+#         lsht_index = rowi[1]['input__filename_index_lshtnns']
+#         nns_index = rowi[0]
+#         
+# #         print(cv_index,'-',lsht_index,'-',nns_index)
+#         cv_file_path = h5_results_filename(dataset_name, 'cv', cv_index).replace('results','time')
+#         lsht_file_path = h5_results_filename(dataset_name, 'lsht', lsht_index).replace('results','time')
+#         lshnns_file_path = h5_results_filename(dataset_name, 'lshnns', nns_index).replace('results','results_evaluation')
+#         lshnns_time_file_path = h5_results_filename(dataset_name, 'lshnns', nns_index).replace('results','time')
+# 
+# #         print('\t',cv_file_path)
+# #         print('\t',lsht_file_path)
+# #         print('\t',lshnns_file_path)
+# #         print('=========')
+#         approach_precisions = hdf_to_sparse_matrix('precisions', lshnns_file_path)
+#         approach_recalls = hdf_to_sparse_matrix('recalls', lshnns_file_path)
+#         average_precision = hdf_to_sparse_matrix('average_precisions', lshnns_file_path).todense()
+#         
+#         b.loc[nns_index,'MAP'] = average_precision.mean()
+#         b.loc[nns_index,'MAP_std'] = average_precision.std()
+#         b.loc[nns_index,'precision_recall_path'] = lshnns_file_path
+#         b.loc[nns_index,'recall_mean'] = approach_recalls[:,-1].todense().mean()
+#         b.loc[nns_index,'recall_std'] = approach_recalls[:,-1].todense().std()
+#         b.loc[nns_index,'precision_mean'] = approach_precisions[:,-1].todense().mean()
+#         b.loc[nns_index,'precision_std'] = approach_precisions[:,-1].todense().std()
+#         
+#         del approach_precisions, approach_recalls, average_precision
+# 
+#         b.loc[nns_index,'documents_count'] = documents_count
+#         b.loc[nns_index,'queries_count'] = queries_count
+#         
+#         with open(cv_file_path.replace('time.h5', 'vocabulary.pkl'),'rb') as f:
+#             b.loc[nns_index,'vocabulary_size'] = len(pickle.load(f))
+# 
+#         q = hdf_to_sparse_matrix('queries', lsht_file_path.replace('time','results'))
+#         b.loc[nns_index,'lsht_features'] = q.shape[1]
+#         del q
+#         
+#         b.loc[nns_index,'indexing_mean_time'] = 0
+#         b.loc[nns_index,'querying_mean_time'] = 0
+#         
+#         cv_time_dataframe = pd.read_hdf(cv_file_path, 'time_dataframe')
+#         b.loc[nns_index,'cv_documents_mean_time'] = cv_time_dataframe.loc[0,'documents_mean_time'] 
+#         b.loc[nns_index,'cv_queries_mean_time'] = cv_time_dataframe.loc[0,'queries_mean_time']
+#                 
+#         b.loc[nns_index,'indexing_mean_time'] += b.loc[nns_index,'cv_documents_mean_time']
+#         b.loc[nns_index,'querying_mean_time'] += b.loc[nns_index,'cv_queries_mean_time']
+#         del cv_time_dataframe 
+# 
+#         d_time = hdf_to_sparse_matrix('documents_time',lsht_file_path)
+#         q_time = hdf_to_sparse_matrix('queries_time',lsht_file_path)
+#         
+#         b.loc[nns_index,'lsht_documents_mean_time'] = d_time.sum(axis=1).mean() 
+#         b.loc[nns_index,'lsht_queries_mean_time'] = q_time.sum(axis=1).mean()
+# 
+#         b.loc[nns_index,'indexing_mean_time'] += b.loc[nns_index,'lsht_documents_mean_time']
+#         b.loc[nns_index,'querying_mean_time'] += b.loc[nns_index,'lsht_queries_mean_time']
+#         del d_time, q_time 
+# 
+#         nns_time_dataframe = pd.read_hdf(lshnns_time_file_path, 'time_dataframe')
+#         b.loc[nns_index,'nns_documents_mean_time'] = nns_time_dataframe.loc[0,'documents_mean_time'] 
+#         b.loc[nns_index,'nns_queries_mean_time'] = nns_time_dataframe.loc[0,'queries_mean_time']
+# 
+#         b.loc[nns_index,'indexing_mean_time'] += b.loc[nns_index,'nns_documents_mean_time']
+#         b.loc[nns_index,'querying_mean_time'] += b.loc[nns_index,'nns_queries_mean_time']
+#         del nns_time_dataframe
+# 
+#         print(b.loc[nns_index,'lsht__selection_function'],' : ',int(b.loc[nns_index,'lsht_features']),' features x ',b.loc[nns_index,'lsht__n_permutations'],' permutation')
+#         print("MAP = %4.2f[+-%4.2f]"%(b.loc[nns_index,'MAP'],b.loc[nns_index,'MAP_std']))
+#         print("recall = %4.2f[+-%4.2f]"%(b.loc[nns_index,'recall_mean'],b.loc[nns_index,'recall_std']))
+#         print("index time = %4.4f"%(b.loc[nns_index,'cv_documents_mean_time']+b.loc[nns_index,'lsht_documents_mean_time']+b.loc[nns_index,'nns_documents_mean_time']))
+#         print("query time = %4.4f"%(b.loc[nns_index,'cv_queries_mean_time']+b.loc[nns_index,'lsht_queries_mean_time']+b.loc[nns_index,'nns_queries_mean_time']))
+#          
+#         print("---->",b.loc[nns_index,'indexing_mean_time'])
+#     b.to_csv('%s%s_lsh_results.csv'%(today,dataset_name),sep='\t')
+#     
+#     del b
 #     
 #     
 # #     '''
